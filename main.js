35b07b823b3906b594e9898a8f4bf42dde3f4e6d
AnnaDavydenko <annadavydenko2544737@gmail.com>
2020-11-07 15:56:59 +0300

    feat: add main page

---
+++ main.js
@@ -0,0 +1,75 @@
+var slideIndex = 1;
+var slideIndex1 = 1;
+var slideIndexMin = 1;
+var slideIndex1Text = 1;
+var slideIndexMinText = 1;
+
+showDivs(slideIndex, slideIndex, ".sliderImg");
+showDivs(slideIndex1, slideIndex1, ".primaryScreen-img");
+showDivs(slideIndex1Text, slideIndex1Text, ".primaryScreen__content");
+showDivs(slideIndexMin, slideIndexMin, ".primaryScreenMin-img");
+showDivs(slideIndexMinText, slideIndexMinText, ".primaryScreenMin__content");
+
+function plusDivs(n) {
+  slideIndex = showDivs(slideIndex += n, slideIndex, ".sliderImg");
+}
+
+function plusDivs1(n) {
+  slideIndex1 = showDivs(slideIndex1 += n, slideIndex1, ".primaryScreen-img");
+  slideIndex1Text = showDivs(slideIndex1Text += n, slideIndex1Text, ".primaryScreen__content");
+  slideIndexMin = showDivs(slideIndexMin += n, slideIndexMin, ".primaryScreenMin-img");
+  slideIndexMinText = showDivs(slideIndexMinText += n, slideIndexMinText, ".primaryScreenMin__content");
+}
+
+function showDivs(n, index, selector) {
+  var x = document.querySelectorAll(selector);
+  if (n > x.length) {
+    index = 1;
+  }
+  if (n < 1) {
+    index = x.length;
+  }
+  for (let i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[index-1].style.display = "block";
+  return index;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const burgerMenu = document.querySelector(".burger");
+const nav = document.querySelector("nav");
+const shadow = document.querySelector(".shadow");
+// const logo =  document.querySelector(".logo");
+
+burgerMenu.addEventListener('click',function(){
+  burgerMenu.classList.toggle("close");
+  nav.classList.toggle("visible");
+  // logo.classList.toggle("visibleLogo");
+});
+
+shadow.addEventListener("click", (event)=>{
+  if(event.target.classList.contains("shadow")){
+    nav.classList.remove("visible");
+    burgerMenu.classList.remove("close");
+  }
+})
+
+
+
+
